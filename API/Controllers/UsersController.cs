dfcd4ebdc4a9db59f343a638df4460c1fa9e28f7
Yan <jan.kozhyn@gmail.com>
2022-10-27 00:03:52 +0300

    Adding a new API Controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entitites;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+            => _context.Users.ToList();
+        // api/users/3 - get users under 3 id
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+            => _context.Users.Find(id);
+        
+    }
+}
