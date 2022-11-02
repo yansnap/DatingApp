031973341c02cbc9a4d6668eb319083ba568e55e
Yan <37808891+YanKozhyn@users.noreply.github.com>
2022-11-02 20:24:32 +0200

    Client login and register (#4)
    * Creating a nav bar
    
    * Introduction to Angular template forms
    
    * Introduction to Angular services
    
    * Injecting services intro components
    
    * Using conditionals to show and remove content
    
    * Using the angular bootstrap components - dropdown
    
    * Persisting the login
    
    * Using the async pipe
    
    * Adding a home page
    
    * adding a register form
    
    * Parent to child communication
    
    * Child to parent communication
    
    * Hooking up the register method to the service

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  logout() {
+    this.accountService.logout();
+  }
+
+}
