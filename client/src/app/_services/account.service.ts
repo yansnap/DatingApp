2a14586baf7d2268bf98431958de8b2ea0906eaf
Yan <jan.kozhyn@gmail.com>
2022-11-03 12:42:16 +0200

    Adding an Angular route guard

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from '../_models/user';
 
 @Injectable({
