e37f55929f30eff65f2477bbe73d984ca5bc9ec3
Yan <37808891+YanKozhyn@users.noreply.github.com>
2022-11-03 13:00:52 +0200

    Routing in angular (#6)
    * Creating some more components
    
    * Adding the nav links
    
    * Routing in code
    
    * adding a toast service for notifications
    
    * Adding an Angular route guard
    
    * Adding a dummy route
    
    * Adding a new theme
    
    * Tidying up the app module by using a shared module

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -8,23 +10,31 @@ import { AccountService } from '../_services/account.service';
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
-  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
+  providers: [
+    {
+      provide: BsDropdownConfig,
+      useValue: { isAnimated: true, autoClose: true },
+    },
+  ],
 })
 export class NavComponent implements OnInit {
-  model: any = {}
+  model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error)
-    })
+      next: (response) => this.router.navigateByUrl('/members'),
+      error: (error) => {console.log(error), this.toastr.error(error.error)},
+    });
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
-
 }
