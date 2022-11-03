06c94c8d94d3c1984f30d42b9d4295da0019ba7c
Yan <jan.kozhyn@gmail.com>
2022-11-04 01:27:51 +0200

    Adding a server error page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     BrowserModule,
