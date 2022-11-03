2d444c5418a3013831bd63f2f00b5ed690e8a463
Yan <jan.kozhyn@gmail.com>
2022-11-04 01:13:58 +0200

    Validation errors

--- client/src/app/errors/test-errors.component.ts
+++ client/src/app/errors/test-errors.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -42,7 +43,7 @@ export class TestErrorsComponent implements OnInit {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => {console.log(error), this.validationErrors = error}
     })
   }
 
