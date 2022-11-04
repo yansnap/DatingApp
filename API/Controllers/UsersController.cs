82988fa7e0ab1b7d02960f72fdb14be8555357af
Yan <37808891+YanKozhyn@users.noreply.github.com>
2022-11-04 18:25:11 +0200

    Extending the api (#8)
    * redirect to not-found page
    
    * Extending the user entity
    
    * Adding a DateTime extensions to calculate age
    
    * Entity Framework relationships
    
    * Generating seed data
    
    * Seeding data part one
    
    * Seeding data part two
    
    * Creating a repository
    
    * Updating the users controller
    
    * Adding a DTO for members and fix migrations
    
    * Adding AutoMapper
    
    * Using AutoMapper
    
    * Configuration
    
    * Using AutoMapper queryable extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entitites;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-            => await _context.Users.ToListAsync();
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+               =>  Ok(await _userRepository.GetMembersAsync());
+
         // api/users/3 - get user under 3 id
-        [HttpGet("{id}")]
-        [Authorize]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-            => await _context.Users.FindAsync(id);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username) 
+            => await _userRepository.GetMemberAsync(username);
+           
         
+
     }
 }
