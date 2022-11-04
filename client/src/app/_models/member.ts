c272365ff769be51331aca39b7b0fc6a901b4fb1
Yan <jan.kozhyn@gmail.com>
2022-11-05 01:44:14 +0200

    fix bug

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -5,7 +5,7 @@ export interface Member {
         username: string;
         photoUrl: string;
         age: number;
-        knowsAs?: any;
+        knowsAs: string;
         created: Date;
         lastActive: Date;
         gender: string;
