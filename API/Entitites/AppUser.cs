f69c402438211b0c251bd2dfda5c2a9b5b340684
Yan <jan.kozhyn@gmail.com>
2022-11-04 14:05:58 +0200

    Extending the user entity

--- API/Entitites/AppUser.cs
+++ API/Entitites/AppUser.cs
@@ -8,5 +8,17 @@ namespace API.Entitites
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
 
+        public DateTime DateOfBirth { get; set; }
+        public string KnowsAs {get; set;}
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos {get;set;}
+
     }
 }
