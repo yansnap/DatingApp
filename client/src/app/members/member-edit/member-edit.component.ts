f06e393c32e66a7ce25f864c836ed9e94a7d7843
Yan <jan.kozhyn@gmail.com>
2022-11-06 01:24:05 +0200

    Adding a Can Deactivate route guard

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
@@ -13,10 +13,17 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editForm') editForm: NgForm
+  @ViewChild('editForm') editForm: NgForm;
 
   member: Member;
   user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    $event: any
+  ) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(
     private accountService: AccountService,
@@ -24,23 +31,23 @@ export class MemberEditComponent implements OnInit {
     private toastr: ToastrService
   ) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => this.user = user
-    })
+      next: (user) => (this.user = user),
+    });
   }
 
   ngOnInit(): void {
-    this.loadMember()
+    this.loadMember();
   }
 
   loadMember() {
     this.memberService.getMember(this.user.userName).subscribe({
-      next: member => this.member = member
-    })
+      next: (member) => (this.member = member),
+    });
   }
 
   updateMember() {
     console.log(this.member);
-    this.toastr.success('Profile updated successfully')
+    this.toastr.success('Profile updated successfully');
     this.editForm.reset(this.member);
   }
 }
