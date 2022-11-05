910f0baf948d6f35c3169fe043a52a596cac89d4
Yan <jan.kozhyn@gmail.com>
2022-11-05 23:02:24 +0200

    Creating a member edit component

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace API.DTOs
 {
     public class UserDto
     {
+        [JsonPropertyName("username")]
         public string UserName { get; set; }
         public string  Token { get; set; }
     }
