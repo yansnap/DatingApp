4a2c1e74560d9b289aa934aa964f2b661d4c9242
Yan <jan.kozhyn@gmail.com>
2022-11-05 15:09:37 +0200

    Routing to the detailed page

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -16,6 +16,6 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'user/' + username);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
