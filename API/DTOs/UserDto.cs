73c35cc549fe84c89a06b1b4295aefaa33b8e2cc
Yan <37808891+YanKozhyn@users.noreply.github.com>
2022-11-07 02:37:04 +0200

    Reactive forms (#12)
    * Reactive form introduction
    
    * Client side validation
    
    * Adding custom validators
    
    * Validation feedback
    
    * Creating a reusable text input
    
    * Using the form builder service
    
    * Expanding the register form
    
    * Adding a reusable date input
    
    * Updating the API register method
    
    * Client side registration

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -12,6 +12,7 @@ namespace API.DTOs
         public string UserName { get; set; }
         public string  Token { get; set; }
         public string PhotoUrl { get; set; }
+        public string KnownAs { get; set; }
         
     }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,6 +23,8 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { NgxSpinnerModule } from 'node_modules/ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DateInputComponent } from './_forms/date-input/date-input.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +42,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TextInputComponent,
+    DateInputComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,6 +51,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule
   ],
