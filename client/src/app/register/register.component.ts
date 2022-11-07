50b3e3f846c93522f4894032263db1e65e832234
Yan <jan.kozhyn@gmail.com>
2022-11-07 02:36:27 +0200

    Client side registration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -7,6 +7,7 @@ import {
   ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -17,14 +18,15 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(
     private accountService: AccountService,
     private toastr: ToastrService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -66,7 +68,14 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('/members')
+      },
+      error: (error) => {
+        this.validationErrors = error;
+      },
+    });
   }
 
   cancel() {
