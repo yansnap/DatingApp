1aa030f7f9e0325c91be5b8c3cb105fa2fc51886
Yan <jan.kozhyn@gmail.com>
2022-11-22 02:26:36 +0200

    Deleting messages on the API

--- API/Data/MessageRepositroy.cs
+++ API/Data/MessageRepositroy.cs
@@ -44,10 +44,12 @@ namespace API.Data
                 .AsQueryable();
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username),
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username
+                    && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username
+                    && u.SenderDeleted == false),
                 _ => query.Where(u => u.Recipient.UserName ==
-                    messageParams.Username && u.DateRead == null)
+                    messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -63,18 +65,20 @@ namespace API.Data
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(m => m.Recipient.UserName == currentUsername
+                        && m.RecipientDeleted == false
                         && m.Sender.UserName == recipientUsername
                         || m.Recipient.UserName == recipientUsername
-                        && m.Sender.UserName == currentUsername
+                        && m.Sender.UserName == currentUsername 
+                        && m.SenderDeleted == false
                     )
                     .OrderBy(m => m.MessageSent)
                     .ToListAsync();
-            var unreadMessages = messages.Where(m => m.DateRead == null 
+            var unreadMessages = messages.Where(m => m.DateRead == null
                 && m.Recipient.UserName == currentUsername).ToList();
 
             if (unreadMessages.Any())
             {
-                foreach(var message in unreadMessages)
+                foreach (var message in unreadMessages)
                 {
                     message.DateRead = DateTime.Now;
                 }
