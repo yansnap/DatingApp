28e536f472854258b057e4cc4ce38c648818550b
Yan <jan.kozhyn@gmail.com>
2022-11-25 21:48:49 +0200

    Setting up the entitites

--- API/Entitites/AppUser.cs
+++ API/Entitites/AppUser.cs
@@ -1,17 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 namespace API.Entitites
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
-
         public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
         public string Introduction { get; set; }
@@ -20,11 +14,11 @@ namespace API.Entitites
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
-
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     
     }
 }
---
+++ API/Entitites/AppUserRole.cs
@@ -0,0 +1,9 @@
+namespace API.Entitites
+{
+    public class AppUserRole 
+    {
+        public AppUser User {get; set;}
+        public AppRole Role { get; set; }
+
+    }
+}
