868324c7421ff4f70d41051c8d92eb16d11f8d32
Yan <jan.kozhyn@gmail.com>
2022-11-27 22:19:09 +0200

    Adding an admin guard

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -38,6 +38,10 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    console.log(user);
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -45,5 +49,9 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]))
   }
 }
