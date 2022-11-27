82f05fb4e44a9b4c1ae973f114695a4f8743bd62
Yan <jan.kozhyn@gmail.com>
2022-11-27 23:15:28 +0200

    Setting up modals

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { RolesModalComponent } from 'src/app/modals/roles-modal/roles-modal.component';
 import { User } from 'src/app/_models/user';
 import { AdminService } from 'src/app/_services/admin.service';
 
@@ -9,17 +11,34 @@ import { AdminService } from 'src/app/_services/admin.service';
 })
 export class UserManagementComponent implements OnInit {
   users: User[] = [];
+  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService, private modalService: BsModalService) { }
 
   ngOnInit(): void {
     this.getUsersWithRoles();
   }
 
   getUsersWithRoles() {
+
     this.adminService.getUsersWithRoles().subscribe({
       next: users => this.users = users 
     })
+  }
+
+  openRolesModal() {
+    const initialState: ModalOptions = {
+      initialState: {
+        list: [
+          'Do thing',
+          'Another thing',
+          'Something else'
+        ],
+        title: 'Test modal'
+      }
+    }
+    this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    this.bsModalRef.content.closeBtnName = 'Close';
   }
 
 }
