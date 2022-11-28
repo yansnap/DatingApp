051e85d91659813e9ebe4fe88da2817e7ee38d74
Yan <jan.kozhyn@gmail.com>
2022-11-29 01:18:54 +0200

    Tracking the message groups

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -15,6 +15,8 @@ namespace API.Data
         }
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Connection> Connections { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -24,7 +26,7 @@ namespace API.Data
                 .WithOne(u => u.User)
                 .HasForeignKey(ur => ur.UserId)
                 .IsRequired();
-            
+
             builder.Entity<AppRole>()
                 .HasMany(ur => ur.UserRoles)
                 .WithOne(u => u.Role)
